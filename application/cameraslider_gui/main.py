63b62f81fd65399304ac0b288e495d97e6b50847
Dennis Eder <Dennis.eder91@web.de>
2020-05-05 19:52:06 +0200

    second commit

--- application/cameraslider_gui/main.py
+++ application/cameraslider_gui/main.py
@@ -5,6 +5,8 @@ import os
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
 from cameraslider_design import cameraslider_design
 
+# test
+
 class cameraslider_gui(QtWidgets.QMainWindow, cameraslider_design):
     def __init__(self, path):
         self.path = path
