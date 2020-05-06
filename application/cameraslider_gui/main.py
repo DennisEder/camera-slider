7be8038bcc276bdcc3c0d141dba75a6b46427431
Dennis Eder <Dennis.eder91@web.de>
2020-05-06 21:31:58 +0200

    Further developmemt steps of UI

--- application/cameraslider_gui/main.py
+++ application/cameraslider_gui/main.py
@@ -14,12 +14,27 @@ class cameraslider_gui(QtWidgets.QMainWindow, cameraslider_design):
         self.setupUI(self.path)
 
         self.qt_actionConfiguration.triggered.connect(self.switchToConnectionConfiguration)
+        self.qt_actionConnectDisconnect.triggered.connect(self.connectDisconnectDevice)
         self.qt_actionManual.triggered.connect(self.switchToModeManual)
+
+
         self.qt_dial_transVel.valueChanged.connect(self.valueChangeManualTransVel)
         self.qt_dial_rotVel.valueChanged.connect(self.valueChangeManualRotVel)
         self.qt_sl_transPos.valueChanged.connect(self.valueChangeManualTransPos)
         self.qt_dial_rotPos.valueChanged.connect(self.valueChangeManualRotPos)
         self.qt_pb_startSlider.clicked.connect(self.startSliderCurrentSettings)
+
+    def switchToConnectionConfiguration(self):
+        self.qt_stackedWidget.setCurrentIndex(0)
+
+    def switchToModeManual(self):
+        self.qt_stackedWidget.setCurrentIndex(1)
+
+    def connectDisconnectDevice(self):
+        self.qt_statusbar.setStyleSheet("QStatusBar{background-color:rgb(0, 170, 0);}")
+        self.qt_statusbar.showMessage('connected')
+        self.qt_actionConnectDisconnect.setText('Disconnect')
+
 
     def valueChangeManualTransVel(self):
         self.qt_lbl_transVelCurrent.setText(str(self.qt_dial_transVel.value()) + ' rpm')
@@ -33,12 +48,6 @@ class cameraslider_gui(QtWidgets.QMainWindow, cameraslider_design):
     def valueChangeManualRotPos(self):
         self.qt_lbl_rotPosCurrent.setText(str(self.qt_dial_rotPos.value()) + ' deg')
 
-    def switchToConnectionConfiguration(self):
-        self.qt_stackedWidget.setCurrentIndex(0)
-
-    def switchToModeManual(self):
-        self.qt_stackedWidget.setCurrentIndex(1)
-
     def startSliderCurrentSettings(self):
         if self.qt_pb_startSlider.isChecked() == True:
             self.qt_pb_startSlider.setText('Stop')
@@ -48,6 +57,6 @@ class cameraslider_gui(QtWidgets.QMainWindow, cameraslider_design):
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     app.setStyle('Breeze')
-    widget = cameraslider_gui(r'C:\Users\Dennis Eder\Desktop\Dennis\Programmierung\40_Raspberry Pi\00_camera_slider\application\cameraslider_gui')
+    widget = cameraslider_gui(r'C:\Users\Dennis Eder\Desktop\Dennis\Programmierung\40_Raspberry Pi\00_camera_slider\camera-slider\application\cameraslider_gui')
     widget.show()
     sys.exit(app.exec_())
